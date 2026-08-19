b899dc27c09c79e040a12b2744f7988a44bc4fa6
Cepyfrost <cprogress590@gmail.com>
2026-08-19 02:25:44 +0100

    Refactor MainActivity to enhance WebView functionality

--- MainActivity.java
+++ MainActivity.java
@@ -2,29 +2,56 @@ package com.huntersassociation.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.view.View;
 
 public class MainActivity extends Activity {
-    @Override public void onCreate(Bundle savedInstanceState) {
+
+    private WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WebView web = new WebView(this);
+
+        web = new WebView(this);
         setContentView(web);
-        WebSettings s = web.getSettings();
-        s.setJavaScriptEnabled(true);
-        s.setDomStorageEnabled(true);
-        s.setDatabaseEnabled(true);
-        s.setAllowFileAccess(true);
-        s.setBuiltInZoomControls(false);
+
+        web.setVisibility(View.VISIBLE);
+        web.setEnabled(true);
+        web.setClickable(true);
+        web.setFocusable(true);
+        web.setFocusableInTouchMode(true);
+        web.requestFocus();
+
+        WebSettings settings = web.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setSupportZoom(false);
+        settings.setLoadWithOverviewMode(false);
+        settings.setUseWideViewPort(false);
+
         web.setWebViewClient(new WebViewClient());
-        web.loadUrl("file:///android_asset/hunters_association_prototype.html");
+        web.setWebChromeClient(new WebChromeClient());
+
+        web.loadUrl("file:///android_asset/index.html");
     }
 
-    @Override public void onBackPressed() {
-        WebView web = (WebView)findViewById(android.R.id.content).findViewById(android.R.id.content);
-        super.onBackPressed();
+    @Override
+    public void onBackPressed() {
+        if (web.canGoBack()) {
+            web.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
