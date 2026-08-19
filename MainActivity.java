f2cab5db783bb061f0fdc1c1ba3c8e5c1b815f1e
Cepyfrost <cprogress590@gmail.com>
2026-08-19 13:29:32 +0100

    Refactor MainActivity for WebView handling
    Refactored MainActivity to streamline WebView initialization and settings. Updated onBackPressed to correctly handle back navigation.

--- MainActivity.java
+++ MainActivity.java
@@ -2,47 +2,29 @@ package com.huntersassociation.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.graphics.Color;
-import android.webkit.WebChromeClient;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
-
-    private WebView web;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        web = new WebView(this);
-        web.setBackgroundColor(Color.rgb(3, 5, 15));
-
+        WebView web = new WebView(this);
         setContentView(web);
-
-        WebSettings settings = web.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setDatabaseEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
-        settings.setBuiltInZoomControls(false);
-        settings.setDisplayZoomControls(false);
-        settings.setSupportZoom(false);
-
+        WebSettings s = web.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setBuiltInZoomControls(false);
         web.setWebViewClient(new WebViewClient());
-        web.setWebChromeClient(new WebChromeClient());
-
         web.loadUrl("file:///android_asset/hunters_association_prototype.html");
     }
 
-    @Override
-    public void onBackPressed() {
-        if (web.canGoBack()) {
-            web.goBack();
-        } else {
-            super.onBackPressed();
-        }
+    @Override public void onBackPressed() {
+        WebView web = (WebView)findViewById(android.R.id.content).findViewById(android.R.id.content);
+        super.onBackPressed();
     }
 }
