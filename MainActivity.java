ebc5582f8bf2eaff55dd353efef459bdf1e97da0
Cepyfrost <cprogress590@gmail.com>
2026-08-19 07:57:27 +0100

    Refactor WebView setup and remove error handling
    Removed error handling for WebView loading failure and simplified WebViewClient setup.

--- MainActivity.java
+++ MainActivity.java
@@ -4,12 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.graphics.Color;
 import android.webkit.WebChromeClient;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -20,15 +17,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         web = new WebView(this);
-
-        web.setBackgroundColor(Color.rgb(5, 5, 16));
+        web.setBackgroundColor(Color.rgb(3, 5, 15));
 
         setContentView(web);
 
         WebSettings settings = web.getSettings();
-
         settings.setJavaScriptEnabled(true);
-        settings.setJavaScriptCanOpenWindowsAutomatically(true);
         settings.setDomStorageEnabled(true);
         settings.setDatabaseEnabled(true);
         settings.setAllowFileAccess(true);
@@ -36,27 +30,8 @@ public class MainActivity extends Activity {
         settings.setBuiltInZoomControls(false);
         settings.setDisplayZoomControls(false);
         settings.setSupportZoom(false);
-        settings.setLoadWithOverviewMode(false);
-        settings.setUseWideViewPort(false);
 
-        web.setWebViewClient(new WebViewClient() {
-
-            @Override
-            public void onReceivedError(
-                    WebView view,
-                    WebResourceRequest request,
-                    WebResourceError error) {
-
-                if (request.isForMainFrame()) {
-                    Toast.makeText(
-                            MainActivity.this,
-                            "Hunter's Association failed to load.",
-                            Toast.LENGTH_LONG
-                    ).show();
-                }
-            }
-        });
-
+        web.setWebViewClient(new WebViewClient());
         web.setWebChromeClient(new WebChromeClient());
 
         web.loadUrl("file:///android_asset/hunters_association_prototype.html");
@@ -64,7 +39,7 @@ public class MainActivity extends Activity {
 
     @Override
     public void onBackPressed() {
-        if (web != null && web.canGoBack()) {
+        if (web.canGoBack()) {
             web.goBack();
         } else {
             super.onBackPressed();
